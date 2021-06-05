019d3c6ee35c7edb8f1695f215410155e3f8aeb3
kiki <rizqiramdani1000@gmail.com>
2021-06-05 08:48:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if2_10118069_muhamad_rizqi_ramdani/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.utspraktik_if2_10118069_muhamad_rizqi_ramdani;
+/*
+    NIM             : 10118069
+    Nama            : Muhamad Rizqi Ramdani
+    Kelas           : IF2
+    TGl Pengerjaan  : 5 juni 2021
+  */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
